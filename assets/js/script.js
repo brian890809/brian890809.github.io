3ab34faf76529cd99f7cc2b421c245e05676a6bb
brian890809 <47680367+brian890809@users.noreply.github.com>
2019-12-04 11:32:38 -0800

    final proj

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,13 @@ var eng = document.getElementsByClassName('English');
 
 function change_language(){
   if(chin.style.display == "block") {
+    chin.classList.toggle("hidden");
+    chin.style.visibility="visible";
     chin.style.display = "none";
     eng.style.display= "block";
   } else {
+    chin.style.visibility="visible";
+    chin.style.visibility="hidden";
     chin.style.display = "block";
     eng.style.display= "none";
   }
