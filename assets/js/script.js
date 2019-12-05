9769eb75443293cf7178beae450fd2652e9f69d1
brian890809 <47680367+brian890809@users.noreply.github.com>
2019-12-04 17:46:28 -0800

    final proj

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,37 @@
-var chin = document.getElementsByClassName('Chinese');
-var eng = document.getElementsByClassName('English');
+// var chin = document.getElementsByClassName('Chinese');
+// var eng = document.getElementsByClassName('English');
+// var chinStyle = window.getComputedStyle(chin[1]);
+// var engStyle = window.getComputedStyle(eng[1]);
+//
+// // function change_language(){
+// //   if(chinStyle.getPropertyValue("display") == "flex") {
+// //     chin.classList.toggle("hidden");
+// //     chin.style.visibility="visible";
+// //     chin.style.display = "none";
+// //     eng.style.display= "flex";
+// //   } else {
+// //     chin.style.visibility="visible";
+// //     chin.style.visibility="hidden";
+// //     chin.style.display = "flex";
+// //     eng.style.display= "none";
+// //   }
+// // }
+//
+// document.getElementsByClassName("trans_img").onclick = change_language()
 
-
-function change_language(){
-  if(chin.style.display == "block") {
-    chin.classList.toggle("hidden");
-    chin.style.visibility="visible";
-    chin.style.display = "none";
-    eng.style.display= "block";
-  } else {
-    chin.style.visibility="visible";
-    chin.style.visibility="hidden";
-    chin.style.display = "block";
-    eng.style.display= "none";
-  }
-}
-document.getElementsByClassName("trans_img").onclick = change_language()
+$(document).ready(function() {
+  $(".trans_img").click(function(e) {
+    var display = $(".Chinese").css("display");
+    if (display == "flex") {
+      $(".Chinese").css("visibility", "hidden");
+      $(".Chinese").css("visibility", "visible");
+      $(".Chinese").css("display", "none");
+      $(".English").css("display", "flex");
+    } else {
+      $(".Chinese").css("visibility", "hidden");
+      $(".Chinese").css("visibility", "visible");
+      $(".Chinese").css("display", "flex");
+      $(".English").css("display", "none");
+    }
+  })
+})
